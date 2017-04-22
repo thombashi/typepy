f325092a60709a56650fc89b7f6d82770755d70a
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-04-22 17:11:18 +0900

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ PROJECT_NAME = "typepy"
 
 setuptools.setup(
     name=PROJECT_NAME,
-    version="0.0.4",
+    version="0.0.5",
     url="https://github.com/thombashi/{:s}".format(PROJECT_NAME),
 
     author="Tsuyoshi Hombashi",
