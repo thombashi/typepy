61ab5c45ac97e7cb1c923d27282b960bdc468f69
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-07-31 23:25:50 +0900

    Add setup_requires

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     license="MIT License",
     long_description=long_description,
     packages=setuptools.find_packages(exclude=["test*"]),
-    setup_requires=pytest_runner,
+    setup_requires=["setuptools>=20.2.2"] + pytest_runner,
     tests_require=tests_requires,
     extras_require={
         "test": tests_requires,
