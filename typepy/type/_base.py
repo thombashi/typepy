e846258e0c4800326dd0eb6527a610d14d74709f
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-12 23:49:37 +0900

    Fix Appveyor CI

--- typepy/type/_base.py
+++ typepy/type/_base.py
@@ -53,7 +53,6 @@ class AbstractType(TypeCheckerInterface, ValueConverterInterface):
 
     def __repr__(self):
         return ", ".join([
-            "value={}".format(self._data),
             "typename={}".format(self.typename),
             "strict_level={}".format(self._strict_level),
             "is_type={}".format(self.is_type()),
