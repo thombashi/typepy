be0c66bbb083bf96134aae88554baef706b72ae6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-06-30 17:21:23 +0900

    Try to fix CI for macos

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,8 @@ if [ "$TRAVIS_OS_NAME" = "osx" ] && ! python3; then
     # Install Python3 on osx
     brew upgrade python
     pip3 install setuptools --upgrade
-    pip3 install tox
+    pip3 install .[test]
 else
     pip install setuptools --upgrade
-    pip install coveralls tox
+    pip install .[test]
 fi
