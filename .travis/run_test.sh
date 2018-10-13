fe1b5a6a4b8c538bc4f72bd4491df020df0de860
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-13 12:45:33 +0900

    Update a script to run test cases

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
-echo $pyver
 
-tox
+if [ "$TOXENV" != "cov" ] ; then
+    tox
+fi
