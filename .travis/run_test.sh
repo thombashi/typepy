4ca41b77e074395145c46eb2f36c9e75a71a0296
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-13 09:21:01 +0900

    Update CI for coverage

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
+echo $pyver
 
 tox
 
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "36" ]; then
-    python setup.py test --addopts "-v --cov typepy --cov-report term-missing"
-fi
+#if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "cov" ]; then
+#    python setup.py test --addopts "-v --cov typepy --cov-report term-missing"
+#fi
