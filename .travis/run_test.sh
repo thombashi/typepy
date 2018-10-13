15c061ce2272c6bd74094ef3a953474fe1621a2d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-13 09:55:22 +0900

    Remove comments

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -4,7 +4,3 @@ pyver=$(python -c "from __future__ import print_function; import sys; print('{}{
 echo $pyver
 
 tox
-
-#if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "cov" ]; then
-#    python setup.py test --addopts "-v --cov typepy --cov-report term-missing"
-#fi
