385908430914868ad21c197c0bbb80f051b01452
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-03 10:08:48 +0900

    Add package dependency version constraints

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
 SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
 DATETIME_REQUIRES = [
-    "python-dateutil>=2.7.5",
+    "python-dateutil>=2.7.5,<3.0.0",
     "pytz>=2018.7",
 ]
 
