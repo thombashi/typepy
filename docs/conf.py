da11a18caa5d005e8206d10fcf7c9921f0b736f1
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-30 15:17:54 +0900

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,14 @@
 import os
 import sys
 
-import sphinx_rtd_theme
 from typepy import __author__, __copyright__, __name__, __version__
+
+
+try:
+    import sphinx_rtd_theme
+    RTD_THEME_INSTALLED = True
+except ImportError:
+    RTD_THEME_INSTALLED = False
 
 
 sys.path.insert(0, os.path.abspath('../typepy'))
@@ -78,7 +84,8 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if RTD_THEME_INSTALLED:
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
