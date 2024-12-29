eae498fa70f371da2c7c0d5322403d1be72b74ee
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-12-29 14:27:50 +0900

    Add Changelog link to project URLs in setup.py
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["test*"]),
     package_data={MODULE_NAME: ["py.typed"]},
     project_urls={
+        "Changelog": f"{REPOSITORY_URL:s}/releases",
         "Documentation": f"https://{MODULE_NAME:s}.rtfd.io/",
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
