7ad552ffae342f2b8c7af682d5844199e7d5c771
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-12-29 18:15:35 +0900

    Bump version to 1.3.4
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- typepy/__version__.py
+++ typepy/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = f"Copyright 2017-2024, {__author__}"
 __license__ = "MIT License"
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
